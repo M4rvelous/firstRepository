1a904be65d849cf7432f65e97ab19e378b54ca5a
Nick Moumouris <nickmoumouris@gmail.com>
2017-05-10 22:03:51 +0200

    Update client.java

--- restaurantReservation/client.java
+++ restaurantReservation/client.java
@@ -17,7 +17,7 @@ ArrayList times = new ArrayList();
 ArrayList tables = new ArrayList();
 ArrayList tableNums = new ArrayList();
 ArrayList persons = new ArrayList();
-
+ArrayList sumOfPeople = new ArrayList();
 
 public void display() {
     out.println(fixedData[0]+ names);
@@ -25,5 +25,33 @@ public void display() {
     out.println(fixedData[2]+tableNums);
     out.println(fixedData[3]+persons);
 }
+
+public void displayNames(){
+    out.println(fixedData[0]+ names);
+}
+
+
+public void sumClients(){
+    int sum = 0 ;
+    
+    for (int i=0 ; i < persons.size(); i++){
+    sum = sum + persons.get(i);
+}
+  out.println("Total Clients:"+sum);
+  persons.clear();              // clears Arraylist, should remain there?
+}
+
+public void sumOfTables(){
+    int sum =  tableNums.size();       // sum of reserved tables for the night
+    
+    ArrayList tableCounters = new ArrayList();
+    
+    for (int i=0; i< tableNums.size(); i++){
+      while  (tableCounters.contains(tableNums.get(i))) {
+           tableCounters.set(i,tableNums.get(i));
+      }
+    }
+}
+
 
 }
