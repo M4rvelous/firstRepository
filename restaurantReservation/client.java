facb069fddac72b7e059ac7a027dedf2bdd67f1f
Nick Moumouris <nickmoumouris@gmail.com>
2017-05-11 20:26:48 +0200

    Update client.java

--- restaurantReservation/client.java
+++ restaurantReservation/client.java
@@ -12,12 +12,11 @@ import java.util.*;
 public class client {
 
 String fixedData[] ={"Name:", "Time of Reservation:", "Table Number:", "People:"};
-ArrayList names = new ArrayList();
-ArrayList times = new ArrayList();
-ArrayList tables = new ArrayList();
-ArrayList tableNums = new ArrayList();
-ArrayList persons = new ArrayList();
-ArrayList sumOfPeople = new ArrayList();
+ArrayList<String> names = new ArrayList();
+ArrayList<Double> times = new ArrayList();
+ArrayList<Integer> tables = new ArrayList();
+ArrayList<Integer> tableNums = new ArrayList();
+ArrayList<Integer> persons = new ArrayList();
 
 public void display() {
     out.println(fixedData[0]+ names);
@@ -26,32 +25,63 @@ public void display() {
     out.println(fixedData[3]+persons);
 }
 
-public void displayNames(){
-    out.println(fixedData[0]+ names);
+public void displayNames(){                 //display names of clients that resereved today
+    for (int i= 0 ; i < names.size(); i++){
+    out.println(fixedData[0]+ names.get(i));
+    }
 }
 
-
-public void sumClients(){
-    int sum = 0 ;
-    
+public void sumClients(){               //sums the clients for that evening 
+    int sum  = 0 ; 
     for (int i=0 ; i < persons.size(); i++){
     sum = sum + persons.get(i);
 }
   out.println("Total Clients:"+sum);
-  persons.clear();              // clears Arraylist, should remain there?
+  persons.clear();                     // clears Arraylist, should remain there?
 }
 
-public void sumOfTables(){
-    int sum =  tableNums.size();       // sum of reserved tables for the night
+int[] tablesArray = { 0,0,0,0,0,0,0,0,0,0}; 
+int sum;
+public void sumOfTables(){                 //sums reserved tables and sorts how many times a table was reserved that night
+     sum = tableNums.size();
     
-    ArrayList tableCounters = new ArrayList();
-    
-    for (int i=0; i< tableNums.size(); i++){
-      while  (tableCounters.contains(tableNums.get(i))) {
-           tableCounters.set(i,tableNums.get(i));
-      }
+    for (int i=0; i < tableNums.size(); i++){
+    if (tableNums.equals(1)){
+        tablesArray[0] ++ ;
     }
+    else if (tableNums.equals(2)) {
+        tablesArray[1] ++ ;
+    }
+    else if (tableNums.equals(3)) {
+        tablesArray[2] ++ ;
+    }
+      else if (tableNums.equals(4)) {
+        tablesArray[3] ++ ;
+    }
+    else if (tableNums.equals(5)) {
+        tablesArray[4] ++ ;
 }
+    else if (tableNums.equals(6)) {
+        tablesArray[5] ++ ;
+    }
+    else if (tableNums.equals(7)) {
+        tablesArray[6] ++ ;
+    }
+      else if (tableNums.equals(8)) {
+        tablesArray[7] ++ ;
+    }
+    else if (tableNums.equals(9)) {
+        tablesArray[8] ++ ;
+    }
+     else if (tableNums.equals(10)) {
+        tablesArray[9] ++ ;
+}
+            else{}}}
 
+  public void tablePercentages(){                     // percentahges on table popularity for the night
+       for (int i=0; i < 10; i++) {
+      out.println("Table"+tablesArray[i]+":"+(tablesArray[i]*100)/sum + "%");
+  }
+  }
 
 }
